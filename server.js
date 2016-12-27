c40d6b627900a69ccac02b0cc5dfe9a08a728ae2
delf1 <shayan.imran1996@gmail.com>
2016-12-27 19:16:27 +0500

    added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var parser = require('body-parser');
+var routes = require('./routes');
+
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json());
+
+var port = 3000;
+
+app.use('/', routes);
+app.listen(port);
+
+console.log('Listening on local host port: ' + port);
