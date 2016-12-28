ab1e9c020c65caf18bdc81ea4c9c4919540a9d0a
delf1 <shayan.imran1996@gmail.com>
2016-12-28 12:39:14 +0500

    added preliminary routes.js

---
+++ routes.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+
+var dburl = 'mongo://localhost/favex' //url to access mongo database
+
+router.use(function timeLog(req, res, next) {
+    var date = new Date(Date.now());
+    console.log('Request Recieved: ' + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ' ' + (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+    next();
+}); //logs every request recieved in console, should add request content type checking
+
+router.route('/addUser').post(function (req, res) {
+    
+    mongo.connect(dburl, function (err, db) {
+        if (err) {
+            res.end(err);
+            console.log(err);
+            return;
+        }
+        req.body
+        var users = db.collection('users');
+        
+    })
+});
