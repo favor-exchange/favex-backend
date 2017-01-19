59c19219272e5a6a3c04b40d3b7a093935ef2ffa
tavishcode <tavish.gobindram@gmail.com>
2017-01-20 06:17:03 +0700

    updated nearbyfavors search and json object format

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
-//var dburl = 'mongodb://localhost/favex'; //url to access mongo database
 var dburl= require('./apiKeys.js').mongoLabUrl;
 var googleMapsKey = require('./apiKeys.js').googleMapsKey;
 var googleMapsClient = require('@google/maps').createClient(
@@ -68,9 +67,6 @@ router.route('/addFavor').post(function (req, res) {
             }
             db.close();
         });
-
-        var users = db.collection('users');
-
     });
 });
 
@@ -191,86 +187,218 @@ router.route('/getFavorsDone').get(function (req, res) {
 });
 
 router.route('/getNearbyFavors').get(function (req, res) {
-    var lat = req.query.lat;
-    var lng = req.query.lng;
-    var radius = (req.query.radius != undefined && req.query.radius.length != 0) ?
-        req.query.radius : 500; //uses default value meters when radius n/a
-    if (req.query.lat === undefined || req.query.lng === undefined) {
+    var userLocationId = req.query.userLocationId;
+    var distance = (req.query.distance != undefined && req.query.distance.length != 0) ?
+        req.query.distance : 500; //uses default value meters when radius n/a
+    if (req.query.userLocationId === undefined)
+    {
         res.send(false);
-        console.log('Missing lat or lng parameter');
+        console.log('Missing userLocationId parameter');
         return;
     }
-    else if (req.query.lat.length === 0 || req.query.lng.length === 0) {
+    else if (req.query.userLocationId.length === 0)
+    {
         res.send(false);
-        console.log('lat or lng key is empty');
+        console.log('userLocationId key is empty');
         return;
     }
-    else {
-        var userLocation =
-            {
-                lat: lat,
-                lng: lng
-            };
+    else
+    {
         mongo.connect(dburl, function (err, db) {
-            if (err) {
+        if (err)
+        {
+            res.send(false);
+            console.log(err);
+            db.close();
+            return;
+         }
+        var favors = db.collection('favors');
+        favors.find({ "doerId": null }).toArray(function (err, openFavors)
+        //above returns favors without doerId fiels or favors with doerId set to null
+        {
+            if (err)
+            {
                 res.send(false);
                 console.log(err);
                 db.close();
                 return;
             }
-            var favors = db.collection('favors');
-            favors.find({ "doerId": null }).toArray(function (err, openFavors)
-            //above returns favors withour doerId fiels or favors with doerId set to null
+            else
             {
-                if (err) {
+                if (openFavors.length === 0)
+                {
                     res.send(false);
-                    console.log(err);
+                    console.log('no favors available');
                     db.close();
                     return;
                 }
-                else {
-                    var favorLocations = [];
-                    for (var i = 0; i < openFavors.length; i++)
-                        favorLocations.push(openFavors[i].locationFavor);
-                    if (favorLocations.length === 0) {
-                        res.send(false);
-                        console.log('no nearby favors available');
-                        db.close();
-                        return;
-                    }
-                    else {
-                        var distanceQuery =
-                            {
-                                origins: userLocation,
-                                destinations: favorLocations,
-                                mode: 'walking'
-                            }
-                        googleMapsClient.distanceMatrix(distanceQuery, function (err, result) {
-                            if (err) {
-                                res.send(false);
-                                console.log(err);
-                                db.close();
-                                return;
-                            }
-                            else {
-                                var nearbyFavors = [];
-                                for (var i = 0; i < result.json.rows[0].elements.length; i++) {
-                                    if (result.json.rows[0].elements[i].distance.value <= radius) {
-                                        nearbyFavors.push(openFavors[i]);
-                                        console.log("");
+                else
+                {
+                    var userCoordinatesQuery=
+                    {
+                        placeid:userLocationId
+                    };
+                    googleMapsClient.place(userCoordinatesQuery, function (err, result) //to find lat and lng of user
+                    {
+                      if (err)
+                      {
+                          res.send(false);
+                          console.log(err);
+                          db.close();
+                          return;
+                      }
+                      var userLat=result.json.result.geometry.location.lat;
+                      var userLng=result.json.result.geometry.location.lng;
+                      var userLocationObj=
+                      {
+                          lat:userLat,
+                          lng:userLng
+                      };
+                      console.log("user coordinates "+userLat+" "+userLng);
+                      var favorLocationObjArray= []; //will store {lat:x,lng:y} for each favor location
+                      var recursive = function populateFavorLocationObjArray(index)
+                      {
+                        if(index===openFavors.length)
+                        {
+                          var distanceFromUserToFavorQuery =
+                          {
+                              origins: userLocationObj,
+                              destinations: favorLocationObjArray,
+                              mode: 'walking'
+                          };
+                          googleMapsClient.distanceMatrix(distanceFromUserToFavorQuery, function (err, result)
+                          {
+                              if (err)
+                              {
+                                  res.send(false);
+                                  console.log(err);
+                                  db.close();
+                                  return;
+                              }
+                              else
+                              {
+                                var distanceArray= []; //stores total distance that doer will travel
+                                for (var j = 0; j < result.json.rows[0].elements.length; j++)
+                                {
+                                    if(!result.json.rows[0].elements[j].hasOwnProperty("distance")
+                                      || result.json.rows[0].elements[j].distance.value>distance) //remove large distances
+                                    {
+                                        console.log(result.json.rows[0].elements);
+                                        console.log(openFavors.splice(j,1));
+                                        favorLocationObjArray.splice(j,1);
                                     }
+                                    else
+                                    {
+                                        distanceArray.push(result.json.rows[0].elements[j].distance.value);
+                                        console.log("pushed");
+                                    }
+                                  }
+                                  var openFavorsLength= openFavors.length; //saved as value will be altered below
+                                  console.log("openfavorslength "+openFavors.length);
+                                  var recursive1= function queryEachDestination(index)
+                                  {
+                                    if(index===openFavorsLength)
+                                    {
+                                      for(var m=0;m<distanceArray.length;m++)
+                                      {
+                                        if(distanceArray[m]>distance)
+                                          openFavors.splice(m,1);
+                                      }
+                                      console.log("completed algorithm");
+                                      res.send(openFavors);
+                                      return;
+                                    }
+                                    var destinationCoordinatesQuery=
+                                    {
+                                        placeid:openFavors[index].locationRecipientId
+                                    };
+                                    googleMapsClient.place(destinationCoordinatesQuery, function (err, result) //to find lat and lng of destination
+                                        {
+                                            if (err)
+                                            {
+                                                res.send(false);
+                                                console.log(err);
+                                                db.close();
+                                                return;
+                                            }
+                                            var destinationLat=result.json.result.geometry.location.lat;
+                                            var destinationLng=result.json.result.geometry.location.lng;
+                                            var destinationObj=
+                                            {
+                                                lat:destinationLat,
+                                                lng:destinationLng
+                                            };
+                                            console.log(destinationObj);
+                                            var distanceFromFavorToRecipientQuery =
+                                            {
+                                                origins: favorLocationObjArray[index],
+                                                destinations: destinationObj,
+                                                mode: 'walking'
+                                            };
+                                            console.log(distanceFromFavorToRecipientQuery);
+                                            googleMapsClient.distanceMatrix(distanceFromFavorToRecipientQuery, function (err, result)
+                                            {
+                                                if (err)
+                                                {
+                                                    res.send(false);
+                                                    console.log(err);
+                                                    db.close();
+                                                    return;
+                                                }
+                                                if (!result.json.rows[0].elements[0].hasOwnProperty("distance")
+                                                    || result.json.rows[0].elements[0].distance.value>distance)
+                                                {
+                                                  openFavors.splice(index,1);
+                                                  distanceArray.splice(index,1);
+                                                }
+                                                else
+                                                {
+                                                    distanceArray[index]=distanceArray[index]+result.json.rows[0].elements[0].distance.value;
+                                                    console.log("distArray "+distanceArray[index])
+                                                }
+                                                console.log("here");
+                                                queryEachDestination(++index);
+                                          });
+                                        });
+                                  }(0);
                                 }
-                                res.send(nearbyFavors);
-                                console.log('nearby favors sent');
-                                db.close();
-                            }
+                              });
+                          return;
+                        }
+                        var favorCoordinatesQuery=
+                        {
+                            placeid:openFavors[index].locationFavorId //contains id of favor location
+                        };
+                        console.log("favor id "+openFavors[index].locationFavorId);
+                        googleMapsClient.place(favorCoordinatesQuery, function (err, result)
+                        {
+                          if (err)
+                          {
+                              res.send(false);
+                              console.log(err);
+                              db.close();
+                              return;
+                          }
+                          var favorLat=result.json.result.geometry.location.lat;
+                          var favorLng=result.json.result.geometry.location.lng;
+                          console.log("favor coordinates "+favorLat+" "+favorLng);
+                          var favorLocationObj=
+                          {
+                                lat:favorLat,
+                                lng:favorLng
+                          };
+                          favorLocationObjArray.push(favorLocationObj);
+                          console.log(favorLocationObjArray);
+                          populateFavorLocationObjArray(++index);
                         });
-                    }
+                      }(0);
+                    });
+                  }
                 }
-            });
-        });
-    }
-});
+              });
+          });
+        }
+      });
 
 router.route('/updateLocation').put(function (req, res) {
     if (req.body.user === undefined) {
