6aa086f013d8df20cc55d71df292372baf9e4c03
delf1 <shayan.imran1996@gmail.com>
2017-01-20 15:23:18 +0500

    updated logger

--- routes.js
+++ routes.js
@@ -25,11 +25,6 @@ const tsFormat = () => (new Date()).toLocaleTimeString();
 const logger = new (winston.Logger)({
   transports: [
     // colorize the output to the console
-    new (winston.transports.Console)({
-      timestamp: tsFormat,
-      colorize: true,
-      level: 'info'
-    }),
     new (require('winston-daily-rotate-file'))({
       filename: `${logDir}/-results.log`,
       timestamp: tsFormat,
